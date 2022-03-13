678033708180ed207043b68456537a35e78cbab8
satkov <g.satkov@mail.ru>
2022-03-13 19:36:10 +0300

    add models to admin panel

--- config/api/admin.py
+++ config/api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listener, Sync, Room
+
+
+@admin.register(Listener)
+class ListenerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email')
+
+
+@admin.register(Sync)
+class SyncAdmin(admin.ModelAdmin):
+    filter_horizontal = ('guests',)
+    list_display = ('id', 'track_id')
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    filter_horizontal = ('guests', 'sync')
+    list_display = ('id', 'name', 'creator', 'rules', 'playlist_id')
