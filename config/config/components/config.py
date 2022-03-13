e4043abdc4c0260e0dc6bdcdb62446178236c355
satkov <g.satkov@mail.ru>
2022-03-13 18:14:20 +0300

    split settings

---
+++ config/config/components/config.py
@@ -0,0 +1,23 @@
+DEBUG = os.environ.get('DEBUG', 'FALSE').upper() == 'TRUE'
+
+ALLOWED_HOSTS = ['*']
+
+INTERNAL_IPS = ["127.0.0.1"]
+
+ROOT_URLCONF = 'config.urls'
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = '/static/'
