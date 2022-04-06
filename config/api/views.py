a0401ff0f44e72bafc20e149cecc6c3658a07c04
satkov <g.satkov@mail.ru>
2022-04-06 18:53:13 +0300

    Добавил возможность получать комнаты, в которых находится пользователь

--- config/api/views.py
+++ config/api/views.py
@@ -1,4 +1,9 @@
-from rest_framework import mixins
+from django.shortcuts import get_object_or_404
+from djangochannelsrestframework.decorators import action
+from rest_framework import mixins, status, viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
 from .models import Listener, Room, Sync
@@ -13,11 +18,7 @@ class ListenerViewSet(mixins.CreateModelMixin,
     queryset = Listener.objects.all()
 
 
-class RoomViewSet(mixins.CreateModelMixin,
-                  mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  mixins.DestroyModelMixin,
-                  GenericViewSet):
+class RoomViewSet(viewsets.ModelViewSet):
     serializer_class = RoomSerializer
     queryset = Room.objects.all()
 
@@ -28,3 +29,15 @@ class SyncViewSet(mixins.CreateModelMixin,
                   GenericViewSet):
     serializer_class = SyncSerializer
     queryset = Sync.objects.all()
+
+
+class GetUsersRooms(APIView):
+    def get(self, request):
+        try:
+            user_id = request.data['user_id']
+        except KeyError:
+            raise ValidationError({'error': 'user_id is required'})
+        listener = get_object_or_404(Listener, id=user_id)
+        rooms = Room.objects.filter(guests=listener)
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
